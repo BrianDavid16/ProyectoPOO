a24e5c8d54d009af4829a0147cca21fadca4686c
Brian Hortua <briandavid.hv@gmail.com>
2018-08-25 14:06:01 -0500

    #1 -> Inicio del proyecto

---
+++ src/Controladores/Vending.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+/**
+ *
+ * @author Emanuel Álvarez, Brian Hortua, Andrés Mora, Thomas Rivera
+ */
+public class Vending {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
